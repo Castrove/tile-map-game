e19f8c3a7a395bd847e8fdc8e919efb5ecb53686
Ludine <ludskieverde@gmail.com>
2021-12-29 13:48:35 +0800

    portals, map levels

--- contents.py
+++ contents.py
@@ -1,39 +1,42 @@
-from pygame.draw import rect
+from pygame import rect
 from settings import *
 import pygame as pg
 
-
+vec = pg.math.Vector2
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game    # to be able to acess all_sprites
         self._layer = PLAYER_LAYER
         self.groups = game.all_sprites  # adding player to all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)    # hard to use super() on classes from imported modules
-        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image = pg.Surface((PLAYER_SIZE, PLAYER_SIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
+        self.vel = vec(0, 0)                # velocity vector instead of vx and vy 
+        self.pos = vec(x, y) * TILESIZE     # position vector instead of x and y
 
-        self.vx, self.vy = 0, 0
+        # self.vx, self.vy = 0, 0
+        self.in_portal = False
 
     def move(self):
         boost = 0
-        self.vx, self.vy = 0, 0
+        # self.vx, self.vy = 0, 0
+        self.vel = vec(0, 0)                # velocity doesn't accumulate, preventing unwanted movement 
+
         keys = pg.key.get_pressed()
         if keys[pg.K_LSHIFT]:
             boost = RUNNING_SPEED
         if keys[pg.K_LEFT]:
-            self.vx = -(PLAYER_SPEED + boost)
+            self.vel.x = -(PLAYER_SPEED + boost)
         if keys[pg.K_RIGHT]:
-            self.vx = (PLAYER_SPEED + boost)
+            self.vel.x = (PLAYER_SPEED + boost)
         if keys[pg.K_UP]:
-            self.vy = -(PLAYER_SPEED + boost)
+            self.vel.y = -(PLAYER_SPEED + boost)
         if keys[pg.K_DOWN]:
-            self.vy= (PLAYER_SPEED + boost)
-        if self.vx != 0 and self.vy != 0:  # prevents faster diagonals
-            self.vx *= .7071
-            self.vy *= .7071
+            self.vel.y= (PLAYER_SPEED + boost)
+        if self.vel.x != 0 and self.vel.y != 0:  # prevents faster diagonals
+            self.vel.x *= .7071
+            self.vel.y *= .7071
 
     # def move(self, dx=0, dy=0):             - for simple movement
          # if not self.collision(dx, dy):
@@ -49,33 +52,47 @@ class Player(pg.sprite.Sprite):
         if dir == 'x':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vx > 0: # sprite moving to the right
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0: # sprite moving to the left
-                    self.x = hits[0].rect.right
-                self.vx = 0                         #stop moving
-                self.rect.x = self.x
+                if self.vel.x > 0: # sprite moving to the right
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0: # sprite moving to the left
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0                         #stop moving
+                self.rect.x = self.pos.x
+
+            enter = pg.sprite.spritecollide(self, self.game.entrances, False)
+            if enter:
+                self.rect.x = enter[0].rect.left
+                self.rect.y = enter[0].rect.top
+                self.in_portal = True
+
+            
         if dir == 'y':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vy > 0: # sprite moving down
-                    self.y = hits[0].rect.top - self.rect.width
-                if self.vy < 0: # sprite moving up
-                    self.y = hits[0].rect.bottom
-                self.vy = 0                         #stop moving
-                self.rect.y = self.y
+                if self.vel.y > 0: # sprite moving down
+                    self.pos.y = hits[0].rect.top - self.rect.width
+                if self.vel.y < 0: # sprite moving up
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0                         #stop moving
+                self.rect.y = self.pos.y
+
+            enter = pg.sprite.spritecollide(self, self.game.entrances, False)
+            if enter:
+                self.rect.x = enter[0].rect.left
+                self.rect.y = enter[0].rect.top
+                self.in_portal = True
+
+
         
 
     def update(self):
         # self.rect.x = self.x * TILESIZE              - for simple by tile movement
         # self.rect.y = self.y * TILESIZE
-
         self.move()
-        self.x += self.vx * self.game.dt    # the speed
-        self.y += self.vy * self.game.dt
-        self.rect.x = self.x
+        self.pos += self.vel * self.game.dt # the speed
+        self.rect.x = self.pos.x
         self.collision('x')
-        self.rect.y = self.y
+        self.rect.y = self.pos.y
         self.collision('y')
 
 
@@ -91,3 +108,62 @@ class Wall(pg.sprite.Sprite):
         self.y = y
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
+
+class Door(pg.sprite.Sprite):
+    portals = []
+
+    def __init__(self, game, x, y, mode):
+        self.game = game
+        self.groups = game.all_sprites, game.entrances
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.rect = self.image.get_rect()
+
+        self.portals.append(self)
+
+        self.mode = mode
+        if self.mode == "entrance": self.image.fill(WHITE)
+        else: self.image.fill(RED) 
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+    def __repr__(self):
+        rep = self.mode
+        return rep
+
+    def activated(self, player):
+        if player.rect.x == self.rect.x and player.rect.y == self.rect.y:
+            return True
+
+    def transport(self, status, player):
+        
+        # if self.mode == "entrance" and status:
+        if self.mode == "entrance" and status and self.activated(player):
+            return 1
+        elif self.mode == "exit" and status and self.activated(player):
+            return -1
+        else: 
+            pass
+    
+    
+        
+
+    # def detect_player(self, status):
+    #     if status:
+    #         return True
+    
+    # def update(self):
+    #     # self.detect_player(Player.in_portal)
+    #     print(Player.in_portal)
+
+    # def player_collide(self, player):
+    #     enter = pg.sprite.spritecollide(player, self.game.entrances, False)
+    #     if enter:
+    #         player.rect.x = self.x
+    #         player.rect.y = self.y
+    #         print("enter")
+
+    # def update(self):
+    #     player_collide()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame as pg
 from input import player_input
 from settings import *
 from contents import *
+from tilemap import *
 
 
 class Game:
@@ -16,33 +17,50 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
-        pg.key.set_repeat(300,100) # by millisecond; repeats the key pressed every interval
+        self.level = 1
+        self.prev_level = self.level
+
+        # pg.key.set_repeat(300,100) # by millisecond; repeats the key pressed every interval
 
 
     def load_data(self):    # load map
-        game_folder = path.dirname(__file__)
-        self.map_data = []
-        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
-            for line in f:
-                self.map_data.append(line)
 
+        game_folder = path.dirname(__file__)
+        map_file = f"map{self.level}.txt"
+
+        print("prev:", self.prev_level)
+        if not path.isfile(map_file):
+
+            self.level = self.prev_level
+            map_file = f"map{self.level}.txt"
+            
+        self.map = Map(path.join(game_folder, map_file))
+    
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.LayeredUpdates()
         self.walls = pg.sprite.LayeredUpdates()
+        self.entrances = pg.sprite.LayeredUpdates()
         self.enemies = pg.sprite.LayeredUpdates()
         self.attacks = pg.sprite.LayeredUpdates()
 
         #spawn wall
         self.load_data()
-        for row, tiles in enumerate(self.map_data):
+        Door.portals.clear()
+
+        for row, tiles in enumerate(self.map.data):     # Make sure tile map has no blank line at the start to prevent issues ! ! ! !
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     Wall(self, col, row)
                 elif tile == 'P':
                     self.player = Player(self, col, row)
+                elif tile == '0':
+                    self.entrance = Door(self, col, row, "entrance")
+                elif tile == "X":
+                    self.exit = Door(self, col, row, "exit")   
 
+        self.camera = Camera(self.map.width, self.map.height)   # spawns camera   
 
         self.run()
 
@@ -70,20 +88,27 @@ class Game:
                 self.running = False   # stops program
             if self.action == "stay":
                 pass
+
+        for portal in Door.portals:
             
-            # if "move" in self.action:
-            #     x, y = self.action["move"]
+            increment = portal.transport(self.player.in_portal, self.player)
             
-            #     self.player.move(dx=x, dy=y)
-            
+            if increment == None:
+                continue
+
+            self.prev_level = self.level
+            self.level += increment
+            self.new()
+                    
     def update(self):
         # Game loop - update portion of the game loop
         self.all_sprites.update()
+        self.camera.update(self.player)
 
     def draw_grid(self):
-        for x in range(0, WIDTH, TILESIZE):
+        for x in range(self.camera.x, WIDTH, TILESIZE):
             pg.draw.line(self.win, LIGHTGREY, (x,0), (x,HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
+        for y in range(self.camera.y, HEIGHT, TILESIZE):
             pg.draw.line(self.win, LIGHTGREY, (0,y), (WIDTH,y))
 
 
@@ -91,7 +116,9 @@ class Game:
         # Game loop - draw
         self.win.fill(BLACK)
         self.draw_grid()
-        self.all_sprites.draw(self.win) # prepares drawing
+        # self.all_sprites.draw(self.win) # prepares drawing
+        for sprite in self.all_sprites:
+            self.win.blit(sprite.image, self.camera.apply(sprite))
 
         pg.display.update()     # presents drawing
 
